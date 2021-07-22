b97c933f138add6611eb8156a241e58dcfe8f002
Ginny <jasmin960427@naver.com>
2021-07-22 23:48:48 +0900

    2573

---
+++ if문/src/if문/윤년.java
@@ -0,0 +1,17 @@
+package if문;
+import java.util.*;
+
+public class 윤년 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		if (n % 4 == 0 && n % 100 != 0 || n % 400 == 0)
+			System.out.println(1);
+		else
+			System.out.println(0);
+	}
+
+}
