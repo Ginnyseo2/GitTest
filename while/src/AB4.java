1aa53d4b7815ff5c0a5f104793c4e3214629b827
Ginny <jasmin960427@naver.com>
2021-07-24 03:16:30 +0900

    submitted

---
+++ while/src/AB4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class AB4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		while(true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+		
+			System.out.println(a + b);
+			
+		}
+	}
+
+}
